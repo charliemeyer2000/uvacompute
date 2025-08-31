0dd052c9ed848775c7da7aeed4ad39044de34fb2
charliemeyer2000 <charlie.g.meyer@icloud.com>
2025-08-30 23:09:58 -0400

    replace with convex

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,5 +1,6 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 import "./globals.css";
+import { ConvexClientProvider } from "./providers/convexClientProvider";
 
 export const metadata: Metadata = {
   title: "uvacompute",
@@ -13,7 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ConvexClientProvider>{children}</ConvexClientProvider>
+      </body>
     </html>
   );
 }
