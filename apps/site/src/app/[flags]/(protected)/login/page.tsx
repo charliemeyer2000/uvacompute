963e638fbca63146c1d88d57c8c4a3e9a87df933
charliemeyer2000 <charlie.g.meyer@icloud.com>
2025-09-05 13:41:39 -0400

    [fix]: stopping relative paths!!!

--- apps/site/src/app/[flags]/(protected)/login/page.tsx
+++ apps/site/src/app/[flags]/(protected)/login/page.tsx
@@ -175,7 +175,7 @@ export default function LoginPage() {
             <span className="text-sm text-gray-600">
               Don't have an account?{" "}
               <Link
-                href="./signup"
+                href="/signup"
                 className="font-medium text-blue-600 hover:text-blue-500"
               >
                 Sign up
