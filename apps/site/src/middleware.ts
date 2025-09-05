1dbf8843bf0e2866a7d37c0d05ed4d73b4691dd4
charliemeyer2000 <charlie.g.meyer@icloud.com>
2025-09-04 23:35:16 -0400

    [feat]: we launch darkly round here

---
+++ apps/site/src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse, NextRequest } from "next/server";
+import { precompute } from "flags/next";
+import { rootFlags } from "@/lib/flags";
+
+export const config = { matcher: ["/((?!_next|api|\.well-known|.*\\..*).*)"] };
+
+export async function middleware(request: NextRequest) {
+  const segments = request.nextUrl.pathname.split("/").filter(Boolean);
+  const alreadyHasFlags = segments.length > 0 && segments[0].includes(".");
+
+  if (alreadyHasFlags) {
+    return NextResponse.next();
+  }
+
+  const flags = await precompute(rootFlags);
+  const nextUrl = new URL(
+    `/${flags}${request.nextUrl.pathname}${request.nextUrl.search}`,
+    request.url,
+  );
+
+  return NextResponse.rewrite(nextUrl, { request });
+}
