e02e6bafeeb3dfe0e66bee8e287f2937138bfcb3
charliemeyer2000 <charlie.g.meyer@icloud.com>
2025-09-13 20:24:27 -0400

    [update]: organization + making vm manager

--- apps/vm-orchestration-service/lib/vm.go
+++ apps/vm-orchestration-service/lib/vm.go
@@ -2,6 +2,8 @@ package lib
 
 import "vm-orchestration-service/structs"
 
+// Uses incus API to create a VM according to the request and
+// forwards port 22 using tailscale funnels.
 func CreateAndForwardVM(req structs.VMCreationRequest) error {
 	return nil
 }
