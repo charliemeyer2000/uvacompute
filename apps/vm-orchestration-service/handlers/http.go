10c66aa043a4a48043cbdf7fa8e054fbd51bce73
charliemeyer2000 <charlie.g.meyer@icloud.com>
2025-10-15 00:01:42 -0400

    [fix]: fix content-type for health check

--- apps/vm-orchestration-service/handlers/http.go
+++ apps/vm-orchestration-service/handlers/http.go
@@ -12,6 +12,7 @@ import (
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(map[string]string{"status": "alive"})
 }
